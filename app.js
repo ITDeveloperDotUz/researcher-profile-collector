4d6e7b759bdb581247b192456b693e89ee0f632f
Abror Aliboyev <comarket@inbox.ru>
2021-08-02 00:02:43 +0500

    Deployement changes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 app.use(express.json({extended: true}))
 
 app.use('/api', require('./routes'))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+	})
+}
 
 const PORT = config.get('port') || 8080
 
