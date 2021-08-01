da00440aef18f56fe9c40ba129949ddc24b67560
Abror Aliboyev <comarket@inbox.ru>
2021-08-01 06:21:47 +0500

    Fixed dark mode

--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,12 +1,19 @@
 import {useEffect, useState} from "react"
 
 export default function useDarkMode() {
-	const [theme, setTheme] = useState('dark')
+	let tc = localStorage.getItem('themeColor')
+	if (!tc){
+		const mq = window.matchMedia('(prefers-color-scheme: dark)');
+		tc = mq.matches ? 'dark' : 'light'
+	}
+	const [theme, setTheme] = useState(tc)
+
 	const colorTheme = theme === 'light' ? 'dark' : 'light'
 	useEffect(() => {
 			const root = window.document.documentElement
-			root.classList.remove(colorTheme)
 			root.classList.add(theme)
+			root.classList.remove(colorTheme)
+			localStorage.setItem('themeColor', theme)
 		},
 		[theme,colorTheme]
 	)
